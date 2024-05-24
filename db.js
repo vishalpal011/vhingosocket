a9868215f1691fb12ce11af7af2f6f6647a5477e
Vishal Pal <148477793+vishalpal011@users.noreply.github.com>
2024-05-24 16:27:33 +0530

    Update db.js
    new db connection

--- db.js
+++ db.js
@@ -1,33 +1,20 @@
-// // db.js
-// import mysql from 'mysql';
-
-// const db = mysql.createConnection({
-//   host: '51.79.204.225',
-//   port : 3306,
-//   user: 'corecad_vhingo',
-//   password: '123@Vhingo',
-//   database: 'corecad_vhingo'
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL database:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
-
-// export default db;
-
-
 import mysql from 'mysql';
 
+// const db = mysql.createConnection({
+//   host: '51.79.146.36',
+//   port : 3306,
+//   user: 'vhingo_db',
+//   password: '!h@r!7N3z3Lh',
+//   database: 'vhingo_db'
+// });
+
 const db = mysql.createConnection({
-  host: '51.79.146.36',
+  host: '93.127.185.122',
   port : 3306,
-  user: 'vhingo_db',
-  password: '!h@r!7N3z3Lh',
-  database: 'vhingo_db'
+  user: 'vhingo_nodejs3',
+  password: 'sachin1234',
+  database: 'vhingo_db',
+  connectTimeout: 20000
 });
 
 db.connect((err) => {
