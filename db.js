4be5fb8057975e9bb15e501dc78093dbe50cfa7c
Vishal Pal <148477793+vishalpal011@users.noreply.github.com>
2024-05-27 10:42:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -11,8 +11,8 @@ import mysql from 'mysql';
 const db = mysql.createConnection({
   host: '93.127.185.122',
   port : 3306,
-  user: 'vhingo_nodejs3',
-  password: 'sachin1234',
+  user: 'vhingo_socket',
+  password: 'socket123',
   database: 'vhingo_db',
   connectTimeout: 20000
 });
