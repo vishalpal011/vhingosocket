a0d119e26640f1ca202e39626d979fcb45b77727
Vishal Pal <148477793+vishalpal011@users.noreply.github.com>
2024-07-03 16:45:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ import mysql from 'mysql';
 
 const db = mysql.createConnection({
   host: '51.79.204.225',
-  port : 3306,
+  port : 443,
   user: 'blivmi_socket',
   password: '123@socket',
   database: 'blivmi_crm',
