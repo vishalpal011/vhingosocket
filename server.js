daeb34516090eea84a22227aa56956db98b2ee21
Vishal Pal <148477793+vishalpal011@users.noreply.github.com>
2024-07-03 18:15:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -128,7 +128,8 @@ io.on('connection', (socket) => {
     console.log(btnKaMsg.senderName === "USER");
 
     if (btnKaMsg.senderName === "ADMIN") {
-         socket.in(room_id).emit("message received", btnKaMsg);
+        socket.in(room_id).emit("getmessage", btnKaMsg);
+         
         
         // if (btnKaMsg?.vendor_id) {
         //     socket.in(vendor_id).emit("message received", btnKaMsg);
@@ -139,7 +140,7 @@ io.on('connection', (socket) => {
         // }
     }else
     {
-        socket.in(room_id).emit("getmessage", btnKaMsg);
+        socket.in(room_id).emit("message received", btnKaMsg);
     }
 
     // if (btnKaMsg.senderName === "USER" || btnKaMsg.senderName === "VENDOR") {
